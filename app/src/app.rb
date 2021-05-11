bba398cd0590dde5d02e4697281dc83a9ce37ea1
Piper Elinor <piper@lightbyte.net>
2021-05-10 18:15:13 -0600

    ditch rack

--- app/src/app.rb
+++ app/src/app.rb
@@ -2,37 +2,33 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :bind, '0.0.0.0'
+set :port, 3000
 
 class User < ActiveRecord::Base
 end
 
-class App < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :bind, '0.0.0.0'
+before do
+  content_type :json
+end
 
-  before do
-    content_type :json
-  end
+get '/' do
+  'Hello!'
+end
 
-  get '/' do
-    'Hello!'
-  end
+get '/:id' do
+  "Hello params[:id]"
+end
 
-  get '/:id' do
-    "Hello params[:id]"
-  end
+get '/users/?' do
+  @users = User.all
+  @users.to_json
+end
 
-  get '/users/?' do
-    @users = User.all
-    @users.to_json
-  end
+get '/users/:id/?' do
+  @user = User.find_by_id(params[:id])
+  @user.to_json
+end
 
-  get '/users/:id/?' do
-    @user = User.find_by_id(params[:id])
-    @user.to_json
-  end
-
-  post '/users/' do
-    @user = User.create!(params)
-  end
+post '/users/' do
+  @user = User.create!(params)
 end
